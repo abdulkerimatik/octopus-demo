f25691f035fdf4255d5ec90cd9498fb003ceac3e
Abdulkerim Atik <abdulkerim.atik@chainerist.com>
2023-12-05 12:47:29 +0300

    initial commit

---
+++ src/test/java/com/example/octopusdemo/OctopusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.octopusdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctopusDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
